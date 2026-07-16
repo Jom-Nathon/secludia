57622fcd4c3fb64f788bb59506f7a304829ed24f
jom_nathon <nathon.t.jom@gmail.com>
2026-07-17 04:06:07 +0700

    finishing quickstart tutorial and adding 3d shapes example binary to bin

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,9 @@
 use bevy::prelude::*;
+use secludia::hello;
 
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .add_plugins(DefaultPlugins)
+        .add_plugins(hello::HelloPlugin)
         .run();
 }
-
-fn hello_world() {
-    println!("hello world!");
-}
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
-    }
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Elaina Proctor" {
-            name.0 = "Elaina Hume".to_string();
-            break; // We don't need to change any other names.
-        }
-    }
-}
-
